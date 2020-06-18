b34c23ec40724f3cf5bc9141c79b730a9ec2fab6
Gian-Marco Hutter <32538484+giantest@users.noreply.github.com>
2020-06-18 11:16:21 +0200

    Update update_pip.sh
    use my own library for the git install

--- update_pip.sh
+++ update_pip.sh
@@ -3,7 +3,7 @@ set -ex
 
 orig_dir=$(pwd)
 tmp_dir=$(mktemp -d -t ddsp-XXXX)
-git clone https://github.com/magenta/ddsp.git $tmp_dir
+git clone https://giantest:gitguenselat6321@github.com/giantest/ddsp_gm2/ $tmp_dir
 cd $tmp_dir
 
 python setup.py sdist
