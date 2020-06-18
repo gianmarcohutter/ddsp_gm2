d4e56ebc2ab31f6281095b1f1b53e4aef07261a1
Gian-Marco Hutter <32538484+giantest@users.noreply.github.com>
2020-06-18 11:55:52 +0200

    Update setup.py
    changed url to my personal git repo

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     description='Differentiable Digital Signal Processing ',
     author='Google Inc.',
     author_email='no-reply@google.com',
-    url='http://github.com/magenta/ddsp',
+    url='https://giantest:gitguenselat6321@github.com/giantest/ddsp_gm2/',
     license='Apache 2.0',
     packages=setuptools.find_packages(),
     package_data={
