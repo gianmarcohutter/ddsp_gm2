777b820fa68d226589525e5b84ba89c58be7e971
gianmarco <gianmarcohutter@gmail.com>
2020-07-05 18:48:03 +0200

    indetation again

--- ddsp/training/data_preparation/prepare_tfrecord_lib_phonemes.py
+++ ddsp/training/data_preparation/prepare_tfrecord_lib_phonemes.py
@@ -124,14 +124,13 @@ def add_phoneme(ex, sample_rate, frame_rate):
   f0_hz, f0_confidence = spectral_ops_phonemes.compute_f0(audio, sample_rate, frame_rate)
   ex['phoneme'] =f0_hz.astype(np.float32)
   '''
-	beam.metrics.Metrics.counter('prepare-tfrecord', 'get-phoneme').inc()
-	audio = ex['audio']
-	mean_loudness_db = spectral_ops.compute_loudness(audio, sample_rate,
-						       frame_rate, 2048)
-	ex = dict(ex)
-	ex['phoneme'] = mean_loudness_db.astype(np.float32)
+  beam.metrics.Metrics.counter('prepare-tfrecord', 'get-phoneme').inc()
+  audio = ex['audio']
+  mean_loudness_db = spectral_ops.compute_loudness(audio, sample_rate,frame_rate, 2048)
+  ex = dict(ex)
+  ex['phoneme'] = mean_loudness_db.astype(np.float32)
 
-	return ex
+  return ex
 
 
 def split_example(
